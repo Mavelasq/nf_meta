9d3481e241fb91ca429f8e2b96bccd6c3355ff1c
Mavelasq <82198999+Mavelasq@users.noreply.github.com>
2024-03-14 12:45:55 -0500

    3.14.24

--- code/edit_dat.R
+++ code/edit_dat.R
@@ -15,19 +15,36 @@ write.csv(m2, file = "data/data_template.csv")
 
 data_temp <- read.csv("data/data_template.csv")
 
-if (is.na(data_temp[11,65])){
-  
-}
-  
-if (is.na(data_temp[11,67]))
-  
-idxs <- which(is.na(data_temp[11,69:91]))+68
+data_psc <- data_temp
 
-for (i in idxs){
-data_temp$psc_mean_last[11] <- (data_temp$mean_last[11] - data_temp$mean_first[11])/data_temp$mean_first[11]
+#first for pre intervention psc calculations
+#list ids that have pre intervention values
+  
+pre.ids <- which(!is.na(data_psc$mean_pre)) #get indices of participants with mean_pre values
+idx_mean <- which(grepl("mean", colnames(data_psc)[67:91]))+66  #get indices of columns with "mean" in name
+
+for (p in pre.ids){ #loop through participants
+  
+  idxs <- idx_mean[which(!is.na(data_psc[p,idx_mean]))] #get indexes of columns with values in row p
+  
+  for (i in idxs){ #loop through means/columns
+    data_psc[p, i+26] <- (as.numeric(data_psc[p, i]) - data_psc$mean_pre[p])/data_psc$mean_pre[p] #calculate percent signal change and add value to pscBase
+  }
 }
 
-data_temp$psc_mean_last[11] <- (data_temp$mean_last[11] - data_temp$mean_first[11])/data_temp$mean_first[11]
-data_temp$psc_mean_last[11] <- (data_temp$mean_last[11] - data_temp$mean_first[11])/data_temp$mean_first[11]
-data_temp$psc_mean_last[11] <- (data_temp$mean_last[11] - data_temp$mean_first[11])/data_temp$mean_first[11]
-data_temp$psc_mean_last[11] <- (data_temp$mean_last[11] - data_temp$mean_first[11])/data_temp$mean_first[11]
+
+#calculate for first nft values
+pre.ids <- which(!is.na(data_psc$mean_first)) #get indexes of participants with mean_pre values
+idx_mean <- which(grepl("mean", colnames(data_psc)[69:91]))+68  #get indexes of columns with "mean" in name
+
+for (p in pre.ids){ #loop through participants
+  
+  idxs <- idx_mean[which(!is.na(data_psc[p,idx_mean]))] #get indexes of columns with values in row p
+  
+  for (i in idxs){ #loop through means/columns
+    data_psc[p, i+52] <- (as.numeric(data_psc[p, i]) - data_psc$mean_first[p])/data_psc$mean_first[p] #calculate percent signal change and add value to pscFirst
+  }
+}
+
+
+write.csv(data_psc, file = "data/data_psc.csv")
